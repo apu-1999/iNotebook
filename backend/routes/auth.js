e8380bd67f137f2707b69b7df2f0e522e5681f7e
APURVA SINGH <singhapurva42@gmail.com>
2022-08-26 13:01:42 +0530

    Implemented password hashing and JWT authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt =  require("bcryptjs")
+const JWT_SECRET = 'Apurvabrilliantwork';
+const jwt = require('jsonwebtoken')
 
 
 //Create a User using: POST "/api/auth/createuser".Doesn't reuire auth
@@ -26,17 +29,26 @@ router.post('/createuser', [
         if(user){
             res.status(400).json({error : "Sorry! User with this email already exists"})
         }
+        // generate salt to hash password
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password,salt)
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-        })
+            password: secPass
+        });
+        const data = {
+            user : {
+                id : user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
         // .then(user => res.json(user))
         // .catch(err => {
         //     console.log(err)
         //     res.json({error : "Please enter unique value for key", message : err.message})
         // });
-        res.json(user);
+        res.json({authToken});
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Some error occured");
