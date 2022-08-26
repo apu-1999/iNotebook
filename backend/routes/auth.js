01d6392c7115c8fb19f2cbc9484949e5998ada21
APURVA SINGH <singhapurva42@gmail.com>
2022-08-26 11:29:52 +0530

    Implemented login error functionality and created Thunderclient collections

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,8 +4,8 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 
 
-//Create a User using: POST "/api/auth/".Doesn't reuire auth
-router.post('/', [
+//Create a User using: POST "/api/auth/createuser".Doesn't reuire auth
+router.post('/createuser', [
     body('name','Your Name should be atleast 3 characters').isLength({
         min: 3
     }),
@@ -14,21 +14,33 @@ router.post('/', [
         min: 5
     })
 
-] , (req, res) => {
+] , async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => {
-        console.log(err)
-        res.json({error : "Please enter unique value for key", message : err.message})
-    });
-    
+
+    //Check whether the user with this email exists already
+    try {
+        let user = await User.findOne({email : req.body.email})
+        if(user){
+            res.status(400).json({error : "Sorry! User with this email already exists"})
+        }
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        // .then(user => res.json(user))
+        // .catch(err => {
+        //     console.log(err)
+        //     res.json({error : "Please enter unique value for key", message : err.message})
+        // });
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
     
     
 })
